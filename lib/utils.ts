3a7fc16928ccdc07fa1017a5eb05f21ce979afbb
hollowww-dev <157723327+hollowww-dev@users.noreply.github.com>
2024-10-21 01:10:16 +0300

    Types fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,20 +1,19 @@
-import { PRODUCTResult } from "@/sanity.types";
+import { PRODUCTSResult } from "@/sanity.types";
 import { urlFor } from "@/sanity/lib/image";
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 import { Product } from "use-shopping-cart/core";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
 
-export function parseCartItem(product: PRODUCTResult): Product {
+export function parseCartItem(product: PRODUCTSResult[number]): Product {
 	return {
 		id: product.id,
 		name: product.name,
-		description: product.description,
 		image: urlFor(product.image).width(400).height(400).url(),
-		currency: 'EUR',
+		currency: "EUR",
 		price: product.price,
 	};
 }
