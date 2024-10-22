40ea4294db919cdf411d9f9946e64bed3a48afeb
hollowww-dev <157723327+hollowww-dev@users.noreply.github.com>
2024-10-22 22:05:10 +0300

    Responsiveness tweaks

--- components/productDetails.tsx
+++ components/productDetails.tsx
@@ -54,14 +54,14 @@ const ProductDetails = ({ product }: { product: PRODUCTResult }) => {
 					<CarouselPrevious />
 					<CarouselNext />
 				</Carousel>
-				<div className='flex flex-col flex-1 self-center items-center gap-6 lg:gap-12'>
-					<span className='text-4xl'>
+				<div className='flex flex-col lg:self-center gap-6 lg:gap-12'>
+					<span className='text-4xl self-center'>
 						{formatCurrencyString({
 							value: product.price,
 							currency: config.currency,
 						})}
 					</span>
-					<div className='flex flex-col lg:flex-row gap-2'>
+					<div className='flex flex-1 flex-col lg:flex-row gap-2'>
 						<Button size={"lg"} variant={"outline"} onClick={() => handleAddItem()}>
 							Add to cart
 						</Button>
