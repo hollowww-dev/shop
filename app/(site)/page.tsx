d724d83feecc05a350a7bff2aabf96682d4ba6c3
hollowww-dev <157723327+hollowww-dev@users.noreply.github.com>
2024-10-23 22:41:09 +0300

    Types

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,9 +2,10 @@ import { PRODUCTSResult } from "@/sanity.types";
 import { client } from "@/sanity/lib/client";
 import { PRODUCTS } from "@/sanity/lib/queries";
 import Product from "@/components/product";
+import { ProductType } from "@/types";
 
 export default async function Home() {
-	const products = await client.fetch<Promise<PRODUCTSResult>>(
+	const products = await client.fetch<Promise<ProductType[]>>(
 		PRODUCTS,
 		{},
 		{ cache: process.env.NODE_ENV === "development" ? "no-store" : "force-cache" }
@@ -14,7 +15,7 @@ export default async function Home() {
 			<main className='container mx-auto'>
 				<div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6 py-3'>
 					{products.map((product) => (
-						<Product key={product.id} product={product} />
+						<Product key={product._id} product={product} />
 					))}
 				</div>
 			</main>
