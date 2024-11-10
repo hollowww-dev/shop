9f4e7642b4d374ae27c6ab21ebe29d306cd571a9
hollowww-dev <157723327+hollowww-dev@users.noreply.github.com>
2024-11-10 21:57:02 +0200

    test

--- components/products/product.tsx
+++ components/products/product.tsx
@@ -7,42 +7,38 @@ import { Card, CardHeader, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import config from "@/lib/config.preval";
-import { useRouter } from "next/navigation";
 import { ProductType } from "@/types";
 import { Product as ProductSanity } from "@/sanity.types";
+import Link from "next/link";
 
 const Product = ({ product }: { product: ProductType | ProductSanity }) => {
-	const router = useRouter();
-
 	return (
-		<Card
-			key={product._id}
-			className='relative active:shadow-none transition-shadow cursor-pointer'
-			onClick={() => router.push(`${config.siteUrl}/product/${product._id}`)}
-		>
-			<CardHeader>
-				<AspectRatio ratio={1} className='relative'>
-					<Image
-						src={urlFor(product.image).width(300).height(300).url()}
-						placeholder='blur'
-						blurDataURL={urlFor(product.image).width(24).height(24).blur(10).url()}
-						alt={product.name}
-						className='aspect-square object-cover rounded-md'
-						loading='lazy'
-						fill
-					/>
-				</AspectRatio>
-			</CardHeader>
-			<CardContent className='flex flex-col md:flex-row justify-between md:items-center gap-1 lg:gap-2'>
-				<h4>{product.name}</h4>
-				<Badge variant='outline' className='self-center'>
-					{formatCurrencyString({ value: product.price, currency: config.currency })}
+		<Link href={`${config.siteUrl}/product/${product._id}`} prefetch={true}>
+			<Card key={product._id} className='relative active:shadow-none transition-shadow cursor-pointer'>
+				<CardHeader>
+					<AspectRatio ratio={1} className='relative'>
+						<Image
+							src={urlFor(product.image).width(300).height(300).url()}
+							placeholder='blur'
+							blurDataURL={urlFor(product.image).width(24).height(24).blur(10).url()}
+							alt={product.name}
+							className='aspect-square object-cover rounded-md'
+							loading='lazy'
+							fill
+						/>
+					</AspectRatio>
+				</CardHeader>
+				<CardContent className='flex flex-col md:flex-row justify-between md:items-center gap-1 lg:gap-2'>
+					<h4>{product.name}</h4>
+					<Badge variant='outline' className='self-center'>
+						{formatCurrencyString({ value: product.price, currency: config.currency })}
+					</Badge>
+				</CardContent>
+				<Badge variant={product.available ? "secondary" : "default"} className='absolute top-5 right-5'>
+					{product.available ? "Available" : "Sold"}
 				</Badge>
-			</CardContent>
-			<Badge variant={product.available ? "secondary" : "default"} className='absolute top-5 right-5'>
-				{product.available ? "Available" : "Sold"}
-			</Badge>
-		</Card>
+			</Card>
+		</Link>
 	);
 };
 
